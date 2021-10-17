110fb2ca3a622d02eb5ed8b4ee8314fb77c6f79e
brunaK-19 <92686077+brunaK-19@users.noreply.github.com>
2021-10-17 16:22:52 -0300

    upload 17/10/2021

---
+++ atividade portugol.pl
@@ -0,0 +1,39 @@
+programa AREA DO QUADRADO
+	var L1,L2,A: real
+inicio
+	leia(L1)
+	leia(L2)
+	A<-L1*L2
+	escreva(A)
+fim
+	
+programa PERIMETRO DO QUADRADO
+	var L1,L2,L3,L4,P: real
+inicio
+	leia(L1)
+	leia(L2)
+	leia(L3)
+	leia(L4)
+	P<-L1+L2+L3+L4
+	escreva(P)
+fim
+
+programa AREA DO RETANGULO
+	var L,A: real
+inicio
+	leia(L)
+	leia(A)
+	A<-L*A
+	escreva(A)
+fim
+
+programa PERIMETRO DO RETANGULO
+	var L1,A1,L2,A2,P: real
+inicio
+	leia(L1)
+	leia(A1)
+	leia(L2)
+	leia(A2)
+	P<-L1+A1+L2+A2
+	escreva(P)
+fim
